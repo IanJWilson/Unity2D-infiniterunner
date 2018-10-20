0d632bcaedc6b29087e7178e5e3a043dc3912b66
IanJWilson <ianjwilson8@outlook.com>
2018-10-20 01:25:19 -0400

    An infinite runner built in Unity

---
+++ 2D Thingy/Assets/Scripts/Fireball.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public int damage;
+    public float speed;
+    public GameObject effect;
+
+    public Fireball(int damage, float speed, GameObject effect)
+    {
+        this.damage = damage;
+        this.speed = speed;
+        this.effect = effect;
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<Player>().health--;
+            Debug.Log(other.GetComponent<Player>().health);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ 2D Thingy/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed;
+    public float Yincrement;
+    public float maxY;
+    public float minY;
+
+    private Vector2 targetPos;
+
+    public int health;
+
+    public GameObject moveEffect;
+    public Animator camAnim;
+    public Text healthDisplay;
+
+    public GameObject spawner;
+    public GameObject restartDisplay;
+
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+            transform.position = targetPos;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
+            transform.position = targetPos;
+        }
+    }
+}    
